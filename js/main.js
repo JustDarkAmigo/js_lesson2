f30ae7813b9a0c41fe9a101ba6d5919953672ca9
JustDarkAmigo <JustDarkAmigo>
2020-11-29 16:40:49 +0800

    Сделал Слайдер, Анимированный фон, Гамбургер/выпадающее меню.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,6 @@
 
 
+
 /*
 ООП структура страницы. Объекты - элементы на странице
 Имеет иерархическую структуру
@@ -26,13 +27,14 @@ tag.innerHTML = "задаем содержимое тега";
 Стили элемент.style.свойство для переопределения
 
 */
-
+document.body.style.background = "black";
 
 
 
 
 //TODO: 1) поменяй фон header, 2)перезаписать текст внутри кнопки
 
+document.getElementById("header").style.background = "green";
 
 
 //TODO: на все кнопки повешать соответствующие события, на одну сделать динамический вывод количества действий с этими кнопками
@@ -40,3 +42,38 @@ tag.innerHTML = "задаем содержимое тега";
 
 
 //TODO:повторить для событий ввода с клавиатуры, плюс сделать вывод символа, который сейчас нажат. Показывать динамически в другом section
+// btn btn-onClick
+document.getElementById("on_click").value = "нажмииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииииии";
+
+let Flag_menu = false;
+
+document.getElementById("gamburger_menu_show").onclick = function(){
+    if (Flag_menu){
+        document.getElementById("gamburger_menu").style.display = "none";
+    Flag_menu = false;
+    document.getElementById("header").style.background = "green";
+    }
+    else{
+        document.getElementById("gamburger_menu").style.display = "block";
+        Flag_menu = true;
+        document.getElementById("header").style.background = "tomato";
+    }
+};
+
+function hi(name){
+    console.log("Привет!", name ,"рад тебя видеть");
+
+}
+
+
+let names = ["саша","паша","коля"]
+for(let i = 0; i < names.length; i++){
+    hi(names[i])
+}
+
+$('.my-slider').slick({
+      arrows: false,
+    });
+
+
+
